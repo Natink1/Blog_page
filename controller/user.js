4282ffd7cea744089d791f733d94d288bec2c1cd
Natink1 <natink4825@gmail.com>
2025-07-20 18:06:01 +0300

    Create user

--- controller/user.js
+++ controller/user.js
@@ -2,22 +2,38 @@ import users from "../models/user";
 import { userSchema } from "../Schema/user";
 
 export const login = async (req, res) => {
+  try {
+    userSchema.parse(req.body);
 
-    try{
-  userSchema.parse(req.body);
+    const { email, password } = req.body;
 
-  const { email, password, name } = req.body;
-  const user = await post.findById(id);
-
-  if(user){
-    
-
-  }}
- catch (error) {
+    if (user) {
+    }
+  } catch (error) {
     res.status(400).json({
       message: "Enter User Cridential;",
       error: error.message,
-  })
+    });
+  }
 };
 
-export const signup = async (req, res) => {};
+export const createUser = async (req, res) => {
+  userSchema.parse(req.body);
+
+  const { email, password, name } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ error: "User already exists" });
+  }
+};
--- models/user.js
+++ models/user.js
@@ -11,4 +11,10 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 export default mongoose.model("users", userSchema);
